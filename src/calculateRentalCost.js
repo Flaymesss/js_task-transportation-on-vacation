1a0d455a641c8f3ded7faec8037e46e49bbf75d0
Nazar Lukenchuk <lukenchyuk.nadya1987@gmail.com>
2026-08-17 18:48:11 +0800

    add sol

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let result = 0;
+
+  if (days >= 3 && days <= 6) {
+    result = result - 20;
+  }
+
+  if (days >= 7) {
+    result = result - 50;
+  }
+
+  for (let i = 0; i < days; i++) {
+    result += 40;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
